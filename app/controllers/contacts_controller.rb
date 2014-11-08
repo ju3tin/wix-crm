3911117ea698e9d4aef4839be90ee7ea077569cd
Alexander K <knuxus@gmail.com>
2014-11-08 15:15:11 +0000

    some stuff

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -75,6 +75,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :email, :company_name, :job_title, :telephone)
+      params.require(:contact).permit(:first_name, :last_name, :email, :company_name, :job_title, :telephone, :notes)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108125751) do
+ActiveRecord::Schema.define(version: 20141108151109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141108125751) do
     t.integer  "telephone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "notes"
   end
 
 end
